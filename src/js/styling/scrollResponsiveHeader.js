6be4db6455adb27ff0fcd9432fd77ea0012d7d76
Jonathan Philipp <jonathan.philipp@posteo.de>
2024-03-05 22:20:40 +0100

    strange error. commit-to save

--- src/js/styling/scrollResponsiveHeader.js
+++ src/js/styling/scrollResponsiveHeader.js
@@ -1,11 +1,20 @@
 function makeHeaderScrollResponsive(header) {
     
+
+    const changeHeaderToWhite = () => {
+
+    }
     
+    const changeHeaderToOriginal = () => {
+
+    }
 }
 
 export default function styleHeader() {
     const headerElement = document.querySelector("header");
     if (headerElement !== null) {
         makeHeaderScrollResponsive(headerElement);
+    } else {
+        console.log("No header to style!");
     }
 }
